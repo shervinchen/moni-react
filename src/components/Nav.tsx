3a3c4921a43dbb2930df67818053fb72c20514ba
csdoker <csd758371536@qq.com>
2020-05-19 17:10:47 +0800

    添加svg的webpack配置，完成Nav组件基本样式

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import styled from "styled-components";
 
 import { Link } from "react-router-dom";
+require("icons/tag.svg");
+require("icons/money.svg");
+require("icons/statistics.svg");
 
 const NavWrapper = styled.nav`
   line-height: 24px;
@@ -11,7 +14,15 @@ const NavWrapper = styled.nav`
     > li {
       width: calc(100% / 3);
       text-align: center;
-      padding: 16px;
+      padding: 4px 0;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      .icon {
+        width: 24px;
+        height: 24px;
+      }
     }
   }
 `;
@@ -21,12 +32,21 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#tag" />
+          </svg>
           <Link to="/tags">标签</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#money" />
+          </svg>
           <Link to="/money">记账</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#statistics" />
+          </svg>
           <Link to="/statistics">统计</Link>
         </li>
       </ul>
