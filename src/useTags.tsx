9cec11d23725be6602a81f7d2e8674afbd2a3307
csdoker <csd758371536@qq.com>
2020-05-20 21:58:01 +0800

    封装createId函数

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,21 +1,22 @@
 import { useState } from "react";
+import createId from "lib/createId";
 
 const useTags = () => {
   const [tags, setTags] = useState<{ id: number, name: string }[]>([
     {
-      id: 1,
+      id: createId(),
       name: "衣",
     },
     {
-      id: 2,
+      id: createId(),
       name: "食",
     },
     {
-      id: 3,
+      id: createId(),
       name: "住",
     },
     {
-      id: 4,
+      id: createId(),
       name: "行",
     },
   ]);
--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import useTags from 'useTags'
+import createId from "lib/createId";
 
 const Wrapper = styled.section`
   display: flex;
@@ -48,7 +49,7 @@ const TagsSection: React.FC<Props> = (props) => {
   const onAddTag = () => {
     const tagName = window.prompt("新标签的名称为");
     if (tagName !== null) {
-      setTags([...tags, { id: Math.random(), name: tagName}]);
+      setTags([...tags, { id: createId(), name: tagName}]);
     }
   };
   const onToggleTag = (id: number) => {
