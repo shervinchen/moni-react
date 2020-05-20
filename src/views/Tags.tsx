7ae51e6b3aed896ad17cbc2637da01293426c2b0
csdoker <csd758371536@qq.com>
2020-05-20 21:00:03 +0800

    完成标签列表页基本布局、样式

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,12 +1,60 @@
 import React from "react";
 import Layout from "components/Layout";
-import useTags from 'useTags'
+import useTags from "useTags";
+import Icon from "components/Icon";
+import styled from "styled-components";
+
+const TagList = styled.ol`
+  background: #fff;
+  font-size: 16px;
+  padding-left: 16px;
+  > li {
+    display: flex;
+    min-height: 44px;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 1px solid #e6e6e6;
+    svg {
+      width: 18px;
+      height: 18px;
+      color: #666;
+      margin-right: 16px;
+    }
+  }
+`;
+
+const Button = styled.button`
+  background: #767676;
+  color: #fff;
+  border-radius: 4px;
+  border: none;
+  height: 40px;
+  padding: 0 16px;
+`;
+
+const ButtonWrapper = styled.div`
+  margin-top: 28px;
+  padding: 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Tags = () => {
-  const { tags, setTags } = useTags()
+  const { tags, setTags } = useTags();
   return (
     <Layout>
-      <h2>Tags</h2>
+      <TagList>
+        {tags.map((tag) => (
+          <li key={tag}>
+            <span>{tag}</span>
+            <Icon name="right" />
+          </li>
+        ))}
+      </TagList>
+      <ButtonWrapper>
+        <Button>新建标签</Button>
+      </ButtonWrapper>
     </Layout>
   );
 };
