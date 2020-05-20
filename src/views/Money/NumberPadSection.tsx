fa9c40983df7c228e0842ce98f2bc89a4eaad17e
csdoker <csd758371536@qq.com>
2020-05-20 16:26:04 +0800

    重构NumberPadSection，抽离组件

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,111 +1,26 @@
 import React, { useState } from "react";
-import styled from "styled-components";
-
-const Wrapper = styled.section`
-display: flex;
-flex-direction: column;
-> .output {
-  background: #fff;
-  box-shadow: inset 0 -3px 3px -3px rgba(0,0,0,.25), inset 0 3px 3px -3px rgba(0,0,0,.25);
-  font-size: 36px;
-  font-family: Consolas,monospace;
-  padding: 0 16px;
-  text-align: right;
-  line-height: 72px;
-}
-> .pad {
-  > button {
-    width: 25%;
-    height: 64px;
-    float: left;
-    background: transparent;
-    border: none;
-    &.ok {
-      height: 128px;
-      float: right;
-    }
-    &.zero {
-      width: 50%;
-    }
-    &:nth-child(1) {
-      background: #f2f2f2;
-    }
-    &:nth-child(2),
-    &:nth-child(5) {
-      background: #e8e8e8;
-    }
-    &:nth-child(3),
-    &:nth-child(6),
-    &:nth-child(9) {
-      background: #dedede;
-    }
-    &:nth-child(4),
-    &:nth-child(7),
-    &:nth-child(10) {
-      background: #d3d3d3;
-    }
-    &:nth-child(8),
-    &:nth-child(11),
-    &:nth-child(13) {
-      background: #c9c9c9;
-    }
-    &:nth-child(14) {
-      background: #bfbfbf;
-    }
-    &:nth-child(12) {
-      background: #b5b5b5;
-    }
-  }
-}
-`;
+import Wrapper from './NumberPadSection/Wrapper'
+import generateOutput from './NumberPadSection/generateOutput'
 
 const NumberPadSection: React.FC = () => {
-  const [output, setOutput] = useState('0')
-  const remove = () => {
-    if (output.length === 1) {
-      setOutput("0");
-    } else {
-      setOutput(output.slice(0, -1))
-    }
-  }
-  const clear = () => {
-    setOutput("0");
-  }
+  const [output, setOutput] = useState("0");
   const ok = () => {
-    const number = parseFloat(output)
-    console.log(number)
+    const number = parseFloat(output);
+    console.log(number);
     setOutput("0");
-  }
-  const setNumber = (input: string) => {
-    if (output.length === 16) {
-      return;
-    }
-    if (output === "0") {
-      if ("0123456789".indexOf(input) >= 0) {
-        setOutput(input);
-      } else {
-        setOutput(output + input);
-      }
-      return;
-    }
-    if (output.indexOf(".") >= 0 && input === ".") {
-      return;
-    }
-    setOutput(output + input);
-  }
+  };
   const onClickButtonWrapper = (e: React.MouseEvent) => {
-    const button = e.target as HTMLButtonElement
-    const input = button.textContent as string
-    if (input === '删除') {
-      remove()
-    } else if (input === '清空') {
-      clear()
-    } else if (input === 'OK') {
-      ok()
-    } else {
-      setNumber(input)
+    const button = e.target as HTMLButtonElement;
+    const input = button.textContent as string;
+    if (input === null) {
+      return
     }
-  }
+    if (input === "OK") {
+      ok();
+      return
+    }
+    setOutput(generateOutput(output, input));
+  };
   return (
     <Wrapper>
       <div className="output">{output}</div>
@@ -126,7 +41,7 @@ const NumberPadSection: React.FC = () => {
         <button>.</button>
       </div>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default NumberPadSection
+export default NumberPadSection;
