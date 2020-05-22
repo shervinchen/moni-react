1e3d6c4815f3b9463a05f52adc41f250e6f306b4
csdoker <csd758371536@qq.com>
2020-05-22 11:26:02 +0800

    deploy

---
+++ precache-manifest.fd1cbdd20c8b8246e1edcb6b61dfda75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2473e90daf8a9be863cb33506dc7385d",
+    "url": "/moni-react/index.html"
+  },
+  {
+    "revision": "e33930b7456c6960aa85",
+    "url": "/moni-react/static/css/main.d0661179.chunk.css"
+  },
+  {
+    "revision": "7e700d43c605ac332c7d",
+    "url": "/moni-react/static/js/2.d68925af.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/moni-react/static/js/2.d68925af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e33930b7456c6960aa85",
+    "url": "/moni-react/static/js/main.b416330c.chunk.js"
+  },
+  {
+    "revision": "b97960a1b47ac39bef3b",
+    "url": "/moni-react/static/js/runtime-main.8142aebf.js"
+  }
+]);
